91258e75da5acf3ef46cf2670227ef3a5f7b5f3c
Ansh Godha <ag759@cornell.edu>
2020-12-23 15:56:58 +0530

    Basic code for static serving

--- server/email.js
+++ server/email.js
@@ -9,10 +9,28 @@ function send(name, email, message, cb) {
     subject: "🔔 Someone messaged you through your website! 🚀",
     text: `Hi Ansh! \n\n Someone messaged you from your website. \n\nName: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
   };
+
+  const respMessage = {
+    to: email,
+    from: process.env.EMAIL_ADDRESS,
+    subject: "Hey there! I've received your message 😄",
+    text: `Hey ${
+      name.split(" ")[0]
+    },\n\nI've received your message that you put across through my website! I'll get back to you as soon as I can!\n\nBest,\nAnsh`,
+  };
   sgMail
     .send(msg)
-    .then(() => cb(true))
+    .then(() => {
+      sgMail
+        .send(respMessage)
+        .then(() => cb(true))
+        .catch((err) => {
+          console.log("An error occured in sending the response email.");
+          console.log(err);
+        });
+    })
     .catch((err) => {
+      console.log("An error occured in recording the message.");
       console.log(err);
       cb(false);
     });
--- server/index.js
+++ server/index.js
@@ -1,2 +1,30 @@
 const express = require("express");
-const Router = express.Router();
+const bodyParser = require("body-parser");
+const path = require("path");
+const send = require("./email");
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.post("/send", (req, res) => {
+  const { name, email, message } = req.body;
+  send(name, email, message, (bool) => {
+    return bool
+      ? res.status(200).json({ success: true })
+      : res.status(400).json({ success: false });
+  });
+});
+
+if (process.env.NODE_ENV === "production") {
+  // Serve all static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Route requests to CRA
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
