29cea485a3f98709207658166b2c8c1d2c8420d9
Ansh Godha <ag759@cornell.edu>
2020-12-23 22:08:06 +0530

    Add toasts

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,20 +1,35 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
+import Toast from "react-bootstrap/Toast";
+import * as axios from "axios";
 
 export default function Contact() {
   const onFormSubmit = (e) => {
     e.preventDefault();
-    const formData = { name, email, msg };
-    setName("");
-    setEmail("");
-    setMsg("");
+    const formData = { name, email, message: msg };
+    setLoading(true);
+    axios
+      .post("/send", formData)
+      .then(() => {
+        setName("");
+        setEmail("");
+        setMsg("");
+        setLoading(false);
+        setShowToast(true);
+        setSubmitMsg("Successfully sent email!");
+      })
+      .catch((err) => setSubmitMsg("Error: " + err));
     console.log(formData);
   };
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+  const [submitMsg, setSubmitMsg] = useState("");
 
   return (
     <div style={{ width: "60%" }}>
@@ -26,6 +41,7 @@ export default function Contact() {
             placeholder="Enter your name"
             onChange={(e) => setName(e.target.value)}
             value={name}
+            required
           />
         </Form.Group>
         <Form.Group controlId="formGroupEmail">
@@ -35,6 +51,7 @@ export default function Contact() {
             placeholder="Enter email"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
+            required
           />
         </Form.Group>
         <Form.Group controlId="formGroupTextArea">
@@ -44,12 +61,40 @@ export default function Contact() {
             rows={5}
             onChange={(e) => setMsg(e.target.value)}
             value={msg}
+            required
           />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+        <Button
+          style={{ backgroundColor: loading && "lightblue", width: "20%" }}
+          variant="primary"
+          type="submit"
+          disabled={loading}
+        >
+          {loading && (
+            <Spinner
+              style={{ marginRight: 10 }}
+              animation="grow"
+              variant="info"
+              size="sm"
+            />
+          )}
+          {loading ? "Loading..." : "Submit"}
         </Button>
       </Form>
+
+      <Toast
+        onClose={() => setShowToast(false)}
+        show={showToast}
+        delay={3000}
+        autohide
+        style={{
+          position: "fixed",
+          bottom: 20,
+          right: "5%",
+        }}
+      >
+        <Toast.Body>{submitMsg}</Toast.Body>
+      </Toast>
     </div>
   );
 }
