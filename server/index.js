395f031dd17103c44c1c2a855279d29b1e915b49
Ansh Godha <ag759@cornell.edu>
2020-12-24 11:39:09 +0530

    FIx static serve path

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.post("/send", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   // Serve all static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "..", "client/build")));
 
   // Route requests to CRA
   app.get("*", (req, res) => {
